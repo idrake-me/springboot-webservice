6736677466f39da82500aab41805e18ccad33de7
Seungmin OH <idrake@knu.ac.kr>
2021-02-01 21:45:50 +0900

    chapter 5 source code final update

--- src/test/java/dev/idrake/springboot/web/HelloControllerTest.java
+++ src/test/java/dev/idrake/springboot/web/HelloControllerTest.java
@@ -1,9 +1,13 @@
 package dev.idrake.springboot.web;
 
+import dev.idrake.springboot.config.auth.SecurityConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -12,13 +16,17 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest
+@WebMvcTest(controllers = HelloController.class,
+    excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+    })
 public class HelloControllerTest {
 
     @Autowired
     private MockMvc mvc;
 
     @Test
+    @WithMockUser(roles = "USER")
     public void helloTest() throws Exception {
         String hello = "hello";
 
@@ -29,6 +37,7 @@ public class HelloControllerTest {
 
 
     @Test
+    @WithMockUser(roles = "USER")
     public void helloDtoTest() throws Exception {
         String name = "hello";
         int amount = 1000;
